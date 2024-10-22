0551dab9710f0062416fbc8db14d0cded2712fcf
EC2 Default User <ec2-user@ip-172-31-29-149.ap-northeast-1.compute.internal>
2024-10-22 12:44:12 +0000

    〇〇

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,38 @@
 class CommentsController < ApplicationController
-   def create
-    @map = Map.find(params[:map_id])
+  before_action :set_map, only: [:create, :destroy]  # マップを設定
+  before_action :set_comment, only: [:destroy]       # コメントを設定
+
+  def create
     @comment = @map.comments.build(comment_params)
+    @comment.user = current_user
 
     if @comment.save
       redirect_to maps_path, notice: 'コメントが投稿されました。'
     else
       redirect_to maps_path, alert: 'コメントの投稿に失敗しました。'
     end
-   end
+  end
 
   def destroy
-    @map = Map.find(params[:map_id])
-    @comment = @map.comments.find(params[:id])
-    @comment.destroy
-    redirect_to maps_path, notice: 'コメントが削除されました。'
+    if @comment.user == current_user  # コメントの所有者のみが削除できる
+      @comment.destroy
+      redirect_to maps_path, notice: 'コメントが削除されました。'
+    else
+      redirect_to maps_path, alert: 'コメントの削除に失敗しました。'
+    end
   end
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:content, :user_id, :map_id)
+  def set_map
+    @map = Map.find(params[:map_id])  # マップを取得
   end
 
+  def set_comment
+    @comment = @map.comments.find(params[:id])  # コメントを取得
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
